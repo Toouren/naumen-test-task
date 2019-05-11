9fea8353e917a2702fca44c2f81383b7e9a4f933
nvkvasiliy <vases74@gmail.com>
2019-05-11 16:28:09 +0500

    readme and test habbit fixe

--- src/app/wikiapi-worker/wikiapi-worker.service.spec.ts
+++ src/app/wikiapi-worker/wikiapi-worker.service.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 
 import { WikiapiWorkerService } from './wikiapi-worker.service';
 import { HttpHandler, HttpClient } from '@angular/common/http';
-import { ISearchRequest } from '../types';
 
 describe('WikiapiWorkerService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -10,24 +9,9 @@ describe('WikiapiWorkerService', () => {
   }));
 
   let service: WikiapiWorkerService;
-  let mockFirstValidSearchRequest: ISearchRequest;
-  let mockSecondValidSearchRequest: ISearchRequest;
-  let mockEmptySearchRequest: ISearchRequest;
 
   beforeEach(() => {
     service = TestBed.get(WikiapiWorkerService);
-    mockFirstValidSearchRequest = {
-      request: 'test1',
-      locale: 'ru'
-    };
-    mockSecondValidSearchRequest = {
-      request: 'test2',
-      locale: 'ru'
-    };
-    mockEmptySearchRequest = {
-      request: '',
-      locale: 'ru'
-    };
   });
 
   it('should be created', () => {
